515f18ad4c9a30465fdddb5b55a9ba34ea2cbc5e
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-20 21:24:34 +0300

    Wrote SquareCellViewMapGenerator.

--- Assets/Src/GameplayPresenter/Cells/SquareCellsGeneration/SquareCellViewMapGenerator.cs
+++ Assets/Src/GameplayPresenter/Cells/SquareCellsGeneration/SquareCellViewMapGenerator.cs
@@ -1,4 +1,6 @@
-﻿using castledice_game_data_logic;
+﻿using System;
+using System.Collections.Generic;
+using castledice_game_data_logic;
 
 namespace Src.GameplayPresenter.Cells.SquareCellsGeneration
 {
@@ -13,7 +15,27 @@ namespace Src.GameplayPresenter.Cells.SquareCellsGeneration
 
         public CellViewData[,] GetCellViewMap(GameStartData gameStartData)
         {
-            throw new System.NotImplementedException();
+            var squareCellDataMap = SquareCellDataMapGenerator.GetSquareCellDataMap(gameStartData.CellsPresence);
+            var cellViewDataMap = new CellViewData[squareCellDataMap.GetLength(0), squareCellDataMap.GetLength(1)];
+            for (int i = 0; i < squareCellDataMap.GetLength(0); i++)
+            {
+                for (int j = 0; j < squareCellDataMap.GetLength(1); j++)
+                {
+                    var cellData = squareCellDataMap[i, j];
+                    var assetsIdsList = _assetIdProvider.GetAssetIds(cellData);
+                    var assetId = GetRandomElement(assetsIdsList);
+                    cellViewDataMap[i, j] = new CellViewData(assetId, false);
+                }
+            }
+
+            return cellViewDataMap;
+        }
+        
+        private T GetRandomElement<T>(List<T> list)
+        {
+            var rnd = new Random();
+            var randomIndex = rnd.Next(0, list.Count);
+            return list[randomIndex];
         }
     }
 }
