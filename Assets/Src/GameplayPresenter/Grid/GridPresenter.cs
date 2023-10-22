b839c447353252da8db8609123b0533e788da490
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-22 19:45:39 +0300

    Added GridPresenter and tested.

---
+++ Assets/Src/GameplayPresenter/Grid/GridPresenter.cs
@@ -0,0 +1,22 @@
+﻿using castledice_game_data_logic;
+using Src.GameplayView.Grid;
+
+namespace Src.GameplayPresenter.Grid
+{
+    public class GridPresenter
+    {
+        private IGridView _view;
+        private GameStartData _gameStartData;
+        
+        public GridPresenter(IGridView view, GameStartData gameStartData)
+        {
+            _view = view;
+            _gameStartData = gameStartData;
+        }
+        
+        public void GenerateGrid()
+        {
+            _view.GenerateGrid(_gameStartData.CellType, _gameStartData.CellsPresence);
+        }
+    }
+}
---
+++ Assets/Src/GameplayView/Grid/IGridView.cs
@@ -0,0 +1,9 @@
+﻿using castledice_game_logic;
+
+namespace Src.GameplayView.Grid
+{
+    public interface IGridView
+    {
+        void GenerateGrid(CellType cellType, bool[,] cellsPresenceMatrix);
+    }
+}
