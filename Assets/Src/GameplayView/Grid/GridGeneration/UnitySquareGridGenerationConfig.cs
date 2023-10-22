12ed5fab36ddd8d0c1631b8f84830ee8bdecc3b4
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-22 23:41:50 +0300

    Wrote UnitySquareGridGenerator and tested.

---
+++ Assets/Src/GameplayView/Grid/GridGeneration/UnitySquareGridGenerationConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Src.GameplayView.Grid.GridGeneration
+{
+    public class UnitySquareGridGenerationConfig : ScriptableObject, ISquareGridGenerationConfig
+    {
+        [SerializeField] private float cellLength;
+        [SerializeField] private float cellWidth;
+        [SerializeField] private Vector3 startPosition;
+
+        public float CellLength => cellLength;
+        public float CellWidth => cellWidth;
+        public Vector3 StartPosition => startPosition;
+    }
+}
---
+++ Assets/Src/GameplayView/Grid/GridGeneration/UnitySquareGridGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Src.GameplayView.Grid.GridGeneration
+{
+    public class UnitySquareGridGenerator : MonoBehaviour, IGridGenerator
+    {
+        private IGameObjectsGrid _grid;
+        private ISquareGridGenerationConfig _config;
+
+        public void Init(IGameObjectsGrid grid, ISquareGridGenerationConfig config)
+        {
+            _grid = grid;
+            _config = config;
+        }
+        
+        public void GenerateGrid(bool[,] cellsPresenceMatrix)
+        {
+            var startPos = _config.StartPosition;
+            var length = _config.CellLength;
+            var width = _config.CellWidth;
+            for (int i = 0; i < cellsPresenceMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < cellsPresenceMatrix.GetLength(1); j++)
+                {
+                    var go = new GameObject();
+                    var position = startPos + new Vector3(j * length, 0, i * width);
+                    go.transform.position = position;
+                    _grid.AddParent((i, j), go);
+                }
+            }
+        }
+    }
+}
