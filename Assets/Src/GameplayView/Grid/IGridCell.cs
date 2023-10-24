c7ca0849ebaae44fed61603a1f78d59fd23e9598
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-24 23:53:50 +0300

    Wrote new grid interfaces.

---
+++ Assets/Src/GameplayView/Grid/IGridCell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Src.GameplayView.Grid
+{
+    public interface IGridCell : IEnumerable<GameObject>
+    {
+        void AddChild(GameObject child);
+        bool RemoveChild(GameObject child);
+    }
+}
