dd1500c02ee2721c34c489657d416603c2100ea9
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-25 00:06:48 +0300

    Wrote tests for UnityGridCell.

---
+++ Assets/Src/GameplayView/Grid/UnityGridCell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Src.GameplayView.Grid
+{
+    public class UnityGridCell : MonoBehaviour, IGridCell
+    {
+        public IEnumerator<GameObject> GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void AddChild(GameObject child)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveChild(GameObject child)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Tests/PlayMode/UnityGridCellTests.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using Src.GameplayView.Grid;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests.PlayMode
+{
+    public class UnityGridCellTests
+    {
+        [Test]
+        public void Contains_ShouldReturnTrueForGameObject_IfAddChildWasCalled()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+
+            cell.AddChild(child);
+
+            Assert.True(cell.Contains(child));
+        }
+
+        [Test]
+        public void AddChild_ShouldSetCellGameObject_AsParentOfChildGameObject()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+
+            cell.AddChild(child);
+
+            Assert.AreSame(cell.gameObject, child.transform.parent.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator AddChild_ShouldSetChildWorldPosition_ToCellPosition()
+        {
+            var cellPosition = new Vector3(1, 2, 3);
+            var gameObject = new GameObject();
+            gameObject.transform.position = cellPosition;
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+
+            cell.AddChild(child);
+            yield return null;
+
+            Assert.AreEqual(cellPosition, child.transform.position);
+        }
+
+        [Test]
+        public void RemoveChild_ShouldReturnTrue_IfChildWasAddedBefore()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+            cell.AddChild(child);
+
+            var result = cell.RemoveChild(child);
+
+            Assert.True(result);
+        }
+
+        [Test]
+        public void RemoveChild_ShouldReturnFalse_IfChildWasNotAddedBefore()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+
+            var result = cell.RemoveChild(child);
+
+            Assert.False(result);
+        }
+
+        [Test]
+        public void Contains_ShouldReturnFalse_IfChildWasRemoved()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+            cell.AddChild(child);
+
+            cell.RemoveChild(child);
+
+            Assert.False(cell.Contains(child));        
+        }
+
+        [UnityTest]
+        public IEnumerator RemoveChild_ShouldDestroyChildGameObject()
+        {
+            var gameObject = new GameObject();
+            var cell = gameObject.AddComponent<UnityGridCell>();
+            var child = new GameObject();
+            cell.AddChild(child);
+            
+            cell.RemoveChild(child);
+            yield return null;
+            
+            Assert.True(child == null);
+        }
+    }
+}
