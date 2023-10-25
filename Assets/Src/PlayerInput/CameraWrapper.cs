d7ddbd6a4a036ea4b7a5b26cc7df76d3f2e7bbed
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-26 01:53:38 +0300

    Wrote CameraWrapper.

---
+++ Assets/Src/PlayerInput/CameraWrapper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Src.PlayerInput
+{
+    public class CameraWrapper : IRayProvider
+    {
+        private readonly Camera _camera;
+
+        public CameraWrapper(Camera camera)
+        {
+            _camera = camera;
+        }
+
+        public Ray ScreenPointToRay(Vector2 screenPoint)
+        {
+            return _camera.ScreenPointToRay(screenPoint);
+        }
+    }
+}
