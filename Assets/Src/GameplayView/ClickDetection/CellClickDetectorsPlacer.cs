ca1a6c7d784bf63da10224a40470ad1a1c9dd984
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-25 19:25:04 +0300

    Wrote tests for CellClickDetectorsPlacer.

---
+++ Assets/Src/GameplayView/ClickDetection/CellClickDetectorsPlacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Src.GameplayView.Grid;
+
+namespace Src.GameplayView.ClickDetection
+{
+    public class CellClickDetectorsPlacer
+    {
+        private IGrid _grid;
+        private IUnityCellClickDetectorsFactory _detectorsFactory;
+        
+        public CellClickDetectorsPlacer(IGrid grid, IUnityCellClickDetectorsFactory detectorsFactory)
+        {
+            _grid = grid;
+            _detectorsFactory = detectorsFactory;
+        }
+
+        public List<ICellClickDetector> PlaceDetectors()
+        {
+            throw new System.NotImplementedException();
+        } 
+    }
+}
