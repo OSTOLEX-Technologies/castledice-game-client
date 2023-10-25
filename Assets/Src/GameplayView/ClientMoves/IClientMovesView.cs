8504852c6373af321ad92aceacd1100d2d646687
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-25 20:26:05 +0300

    Implemented ClientMovesView and tested.

--- Assets/Src/GameplayView/ClientMoves/IClientMovesView.cs
+++ Assets/Src/GameplayView/ClientMoves/IClientMovesView.cs
@@ -8,19 +8,6 @@ namespace Src.GameplayView.ClientMoves
     public interface IClientMovesView
     {
         void ShowMovesList(List<AbstractMove> moves);
-        
-        /// <summary>
-        /// This method should invoke <see cref="PositionClicked"/>  event
-        /// </summary>
-        /// <param name="position"></param>
-        void ClickOnPosition(Vector2Int position);
-        
-        /// <summary>
-        /// This method should invoke <see cref="MovePicked"/>  event
-        /// </summary>
-        /// <param name="move"></param>
-        void PickMove(AbstractMove move);
-
         event EventHandler<Vector2Int> PositionClicked;
         event EventHandler<AbstractMove> MovePicked;
     }
---
+++ Assets/Tests/EditMode/ClientMovesViewTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using castledice_game_logic.Math;
+using castledice_game_logic.MovesLogic;
+using NUnit.Framework;
+using Src.GameplayView.ClickDetection;
+using Src.GameplayView.ClientMoves;
+using static Tests.ObjectCreationUtility;
+
+namespace Tests.EditMode
+{
+    public class ClientMovesViewTests
+    {
+        private class DetectorForTests : ICellClickDetector
+        {
+            public Vector2Int Position { get; set; }
+            public event EventHandler<Vector2Int> Clicked;
+
+            public void Click()
+            {
+                Clicked?.Invoke(this, Position);
+            }
+        }
+        
+        public static Vector2Int[] Positions = { (0, 0), (1, 0), (2, 0), (0, 1), (1, 1) };
+        
+        [Test]
+        public void PositionClickedEvent_ShouldBeCalled_IfClickedEventOnDetectorInvoked()
+        {
+            var detector = new DetectorForTests();
+            var view = new ClientMovesView(new List<ICellClickDetector> {detector});
+            var eventInvoked = false;
+            view.PositionClicked += (sender, position) => eventInvoked = true;
+            
+            detector.Click();
+            
+            Assert.True(eventInvoked);
+        }
+
+        [Test]
+        public void PositionClickedEvent_ShouldBeInvokedWithPosition_FromClickedEventOnDetector([ValueSource(nameof(Positions))]Vector2Int position)
+        {
+            var detector = new DetectorForTests();
+            detector.Position = position;
+            var view = new ClientMovesView(new List<ICellClickDetector> {detector});
+            Vector2Int actualPosition = default;
+            
+            view.PositionClicked += (sender, pos) => actualPosition = pos;
+            detector.Click();
+            
+            Assert.AreEqual(position, actualPosition);
+        }
+
+        [Test]
+        public void ShowMovesList_ShouldInvokeMovePickedEvent_WithFirstMoveFromGivenList()
+        {
+            var view = new ClientMovesView(new List<ICellClickDetector>());
+            var move = GetMove();
+            var moves = new List<AbstractMove> {move};
+            AbstractMove actualMove = null;
+            
+            view.MovePicked += (sender, m) => actualMove = m;
+            view.ShowMovesList(moves);
+            
+            Assert.AreSame(move, actualMove);
+        }
+        
+        [Test]
+        public void ShowMovesList_ShouldNotInvokeMovePickedEvent_IfGivenEmptyList()
+        {
+            var view = new ClientMovesView(new List<ICellClickDetector>());
+            var moves = new List<AbstractMove>();
+            AbstractMove actualMove = null;
+            
+            view.MovePicked += (sender, m) => actualMove = m;
+            view.ShowMovesList(moves);
+            
+            Assert.IsNull(actualMove);
+        }
+    }
+}
