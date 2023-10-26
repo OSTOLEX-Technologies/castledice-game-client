8ab91dd98fec3cc947833997451ace3b2813a5c1
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-26 23:02:47 +0300

    Wrote tests for PlaceablesConfigProvider.

---
+++ Assets/Src/GameplayPresenter/GameCreation/GameCreationProviders/PlaceablesConfigProvider.cs
@@ -0,0 +1,13 @@
+﻿using castledice_game_data_logic.Content.Placeable;
+using castledice_game_logic.GameConfiguration;
+
+namespace Src.GameplayPresenter.GameCreation.GameCreationProviders
+{
+    public class PlaceablesConfigProvider : IPlaceablesConfigProvider
+    {
+        public PlaceablesConfig GetPlaceablesConfig(PlaceablesConfigData configData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
