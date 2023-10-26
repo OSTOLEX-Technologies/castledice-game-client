96128def6fa77a59c3c4aab5985771b5ad14bde3
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-27 01:17:17 +0300

    Wrote tests for DecksListProvider.

---
+++ Assets/Src/GameplayPresenter/GameCreation/GameCreationProviders/DecksListProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using castledice_game_data_logic;
+using castledice_game_logic.GameObjects;
+
+namespace Src.GameplayPresenter.GameCreation.GameCreationProviders
+{
+    public class DecksListProvider : IDecksListProvider
+    {
+        public IDecksList GetDecksList(List<PlayerDeckData> decksData)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
