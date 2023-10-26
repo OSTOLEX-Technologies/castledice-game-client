a680f398f89e96f214e6636c192a04bcd841ec21
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-27 01:47:42 +0300

    Wrote tests for ContentToCoordinateProvider.

---
+++ Assets/Src/GameplayPresenter/GameCreation/GameCreationProviders/ContentToCoordinateProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using castledice_game_data_logic.Content;
+using castledice_game_data_logic.Content.Generated;
+using castledice_game_logic;
+using castledice_game_logic.BoardGeneration.ContentGeneration;
+
+namespace Src.GameplayPresenter.GameCreation.GameCreationProviders
+{
+    public class ContentToCoordinateProvider : IGeneratedContentDataVisitor<ContentToCoordinate>
+    {
+        private readonly List<Player> _players;
+
+        public ContentToCoordinateProvider(List<Player> players)
+        {
+            _players = players;
+        }
+
+        public ContentToCoordinate GetContentToCoordinate(GeneratedContentData data)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public ContentToCoordinate VisitCastleData(CastleData data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ContentToCoordinate VisitTreeData(TreeData data)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/ContentToCoordinateProviderTest.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using castledice_game_data_logic.Content.Generated;
+using castledice_game_logic;
+using castledice_game_logic.GameObjects;
+using NUnit.Framework;
+using Src.GameplayPresenter.GameCreation.GameCreationProviders;
+using static Tests.ObjectCreationUtility;
+using Vector2Int = castledice_game_logic.Math.Vector2Int;
+using CastleGO = castledice_game_logic.GameObjects.Castle;
+
+namespace Tests.EditMode
+{
+    public class ContentToCoordinateProviderTest
+    {
+        public static Vector2Int[] Positions = { (0, 0), (0, 1), (1, 0), (1, 1) };
+        
+        [Test]
+        public void GetContentToCoordinate_ShouldThrowArgumentException_IfCastleDataWithUnknownIdIsGiven()
+        {
+            var players = new List<Player> {GetPlayer(1), GetPlayer(2) };
+            var castleData = new CastleData((0, 0), 1, 1, 1, 1, 4);
+            var contentToCoordinateProvider = new ContentToCoordinateProvider(players);
+            
+            Assert.Throws<System.ArgumentException>(() => contentToCoordinateProvider.GetContentToCoordinate(castleData));
+        }
+
+        [Test]
+        public void GetContentToCoordinate_ShouldReturnContentToCoordinateWithAppropriatePosition([ValueSource(nameof(Positions))]Vector2Int position)
+        {
+            var players = new List<Player> {GetPlayer(1), GetPlayer(2) };
+            var castleData = new CastleData(position, 1, 1, 1, 1, 4);
+            var contentToCoordinateProvider = new ContentToCoordinateProvider(players);
+            
+            var contentToCoordinate = contentToCoordinateProvider.GetContentToCoordinate(castleData);
+            
+            Assert.That(contentToCoordinate.Coordinate, Is.EqualTo(position));
+        }
+
+        [Test]
+        public void GetContentToCoordinate_ShouldReturnContentToCoordinateWithAppropriateCastle_IfCastleDataGiven()
+        {
+            var players = new List<Player> {GetPlayer(1), GetPlayer(2) };
+            var castleData = new CastleData((0, 0), 1, 2, 3, 4, 1);
+            var contentToCoordinateProvider = new ContentToCoordinateProvider(players);
+            
+            var contentToCoordinate = contentToCoordinateProvider.GetContentToCoordinate(castleData);
+            var content = contentToCoordinate.Content;
+            var castle = content as CastleGO;
+
+            Assert.Equals(castleData.Durability, castle.GetDurability());
+            Assert.Equals(castleData.CastleCaptureHitCost, castle.GetCaptureHitCost(GetPlayer()));
+            Assert.Equals(castleData.OwnerId, castle.GetOwner().Id);
+            castle.Free();
+            Assert.Equals(castleData.FreeDurability, castle.GetDurability());
+            Assert.Equals(castleData.DefaultDurability, castle.GetMaxDurability());
+        }
+
+        [Test]
+        public void GetContentToCoordinate_ShouldReturnContentToCoordinateWithAppropriateTree_IfTreeDataGiven()
+        {
+            var treeData = new TreeData((0, 0), 3, true);
+            var contentToCoordinateProvider = new ContentToCoordinateProvider(new List<Player>());
+            
+            var contentToCoordinate = contentToCoordinateProvider.GetContentToCoordinate(treeData);
+            var content = contentToCoordinate.Content;
+            var tree = content as Tree;
+
+            Assert.Equals(treeData.CanBeRemoved, tree.CanBeRemoved());
+            Assert.Equals(treeData.RemoveCost, tree.GetRemoveCost());
+        }
+    }
+}
