56c1f360793ea435cb74f30c7b6b46d16369c3dc
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-10-27 20:51:16 +0300

    Wrote tests for BoardConfigProviderTests.

--- Assets/Tests/EditMode/BoardConfigProviderTests.cs
+++ Assets/Tests/EditMode/BoardConfigProviderTests.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
 using castledice_game_data_logic.Content.Generated;
-using castledice_game_data_logic.Content.Placeable;
 using castledice_game_logic;
+using castledice_game_logic.BoardGeneration.CellsGeneration;
 using castledice_game_logic.BoardGeneration.ContentGeneration;
 using Moq;
 using NUnit.Framework;
 using Src.GameplayPresenter.GameCreation.GameCreationProviders;
-using Src.GameplayView.Cells;
 using static Tests.ObjectCreationUtility;
 
 
@@ -14,33 +13,88 @@ namespace Tests.EditMode
 {
     public class BoardConfigProviderTests
     {
+        public static CellType[] CellTypes = {CellType.Square, CellType.Triangle};
+        
         [Test]
         public void GetBoardConfig_ShouldReturnBoardConfig_WithSpawnersListFromGivenProvider()
         {
-            var spawnersProvider = new Mock<IContentSpawnersListProvider>();
-            var cellsGeneratorProvider = new Mock<ICellsGeneratorProvider>();
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
             var expectedList = new List<IContentSpawner>();
-            spawnersProvider.Setup(provider => provider.GetContentSpawnersList(It.IsAny<List<GeneratedContentData>>(), It.IsAny<List<Player>>()))
+            spawnersProviderMock.Setup(provider => provider.GetContentSpawnersList(It.IsAny<List<GeneratedContentData>>(), It.IsAny<List<Player>>()))
                 .Returns(expectedList);
-            var boardConfigProvider = new BoardConfigProvider(spawnersProvider.Object, cellsGeneratorProvider.Object);
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
             
             var boardConfig = boardConfigProvider.GetBoardConfig(GetBoardData(), new List<Player>());
             
             Assert.AreSame(expectedList, boardConfig.ContentSpawners);
         }
         
-        /*public void GetBoardConfig_ShouldReturnBoardConfig_WithCellsGeneratorFromGivenProvider()
+        [Test]
+        public void GetBoardConfig_ShouldReturnBoardConfig_WithCellsGeneratorFromGivenProvider()
         {
-            var spawnersProvider = new Mock<IContentSpawnersListProvider>();
-            var cellsGeneratorProvider = new Mock<ICellsGeneratorProvider>();
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
             var expectedGenerator = new Mock<ICellsGenerator>().Object;
-            cellsGeneratorProvider.Setup(provider => provider.GetCellsGenerator(It.IsAny<bool[,]>()))
+            cellsGeneratorProviderMock.Setup(provider => provider.GetCellsGenerator(It.IsAny<bool[,]>()))
                 .Returns(expectedGenerator);
-            var boardConfigProvider = new BoardConfigProvider(spawnersProvider.Object, cellsGeneratorProvider.Object);
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
             
             var boardConfig = boardConfigProvider.GetBoardConfig(GetBoardData(), new List<Player>());
             
-            Assert.AreSame(expectedGenerator.Object, boardConfig.CellsGenerator);
-        }*/
+            Assert.AreSame(expectedGenerator, boardConfig.CellsGenerator);
+        }
+
+        [Test]
+        public void GetBoardConfig_ShouldReturnBoardConfig_WithCellTypeFromGivenBoardData([ValueSource(nameof(CellTypes))] CellType cellType)
+        {
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
+            var boardData = GetBoardData(cellType);
+            
+            var boardConfig = boardConfigProvider.GetBoardConfig(boardData, new List<Player>());
+            
+            Assert.AreEqual(cellType, boardConfig.CellType);
+        }
+
+        [Test]
+        public void GetBoardConfig_ShouldPassGeneratedContentDataListFromBoardData_ToGivenContentSpawnersListProvider()
+        {
+            var boardData = GetBoardData();
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
+            
+            boardConfigProvider.GetBoardConfig(boardData, new List<Player>());
+            
+            spawnersProviderMock.Verify(provider => provider.GetContentSpawnersList(boardData.GeneratedContent, It.IsAny<List<Player>>()), Times.Once);
+        }
+
+        [Test]
+        public void GetBoardConfig_ShouldPassGivenPlayersList_ToGivenContentSpawnersListProvider()
+        {
+            var players = new List<Player>();
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
+            
+            boardConfigProvider.GetBoardConfig(GetBoardData(), players);
+            
+            spawnersProviderMock.Verify(provider => provider.GetContentSpawnersList(It.IsAny<List<GeneratedContentData>>(), players), Times.Once);
+        }
+        
+        [Test]
+        public void GetBoardConfig_ShouldPassCellsPresenceFromBoardData_ToGivenCellsGeneratorProvider()
+        {
+            var boardData = GetBoardData();
+            var spawnersProviderMock = new Mock<IContentSpawnersListProvider>();
+            var cellsGeneratorProviderMock = new Mock<ICellsGeneratorProvider>();
+            var boardConfigProvider = new BoardConfigProvider(spawnersProviderMock.Object, cellsGeneratorProviderMock.Object);
+            
+            boardConfigProvider.GetBoardConfig(boardData, new List<Player>());
+            
+            cellsGeneratorProviderMock.Verify(provider => provider.GetCellsGenerator(boardData.CellsPresence), Times.Once);
+        }
     }
 }
