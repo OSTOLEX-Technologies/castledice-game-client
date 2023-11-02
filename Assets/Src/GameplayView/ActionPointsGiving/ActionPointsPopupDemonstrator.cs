9e32592768bd057089b7c9a4a7d3fda3dbe21070
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 23:01:42 +0200

    Wrote tests for ActionPointsPopupDemonstrator.

---
+++ Assets/Src/GameplayView/ActionPointsGiving/ActionPointsPopupDemonstrator.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Src.GameplayView.CellsContent.ContentCreation;
+
+namespace Src.GameplayView.ActionPointsGiving
+{
+    public class ActionPointsPopupDemonstrator : IActionPointsPopupDemonstrator
+    {
+        private readonly IActionPointsPopupsProvider _popupsProvider;
+        private readonly int _popupDisappearTimeMilliseconds;
+
+        public ActionPointsPopupDemonstrator(IActionPointsPopupsProvider popupsProvider, int popupDisappearTimeMilliseconds)
+        {
+            _popupsProvider = popupsProvider;
+            _popupDisappearTimeMilliseconds = popupDisappearTimeMilliseconds;
+        }
+
+        public void ShowActionPointsPopup(PlayerColor playersColor, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private async Task HidePopup()
+        {
+            
+        }
+    }
+}
