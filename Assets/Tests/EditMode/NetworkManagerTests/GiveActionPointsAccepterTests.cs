0c404ff0779df0cbc9e46d9fb3d0f633b6889a79
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 22:07:27 +0200

    Wrote tests for GiveActionPointsAccepter.

---
+++ Assets/Tests/EditMode/NetworkManagerTests/GiveActionPointsAccepterTests.cs
@@ -0,0 +1,25 @@
+﻿using casltedice_events_logic.ServerToClient;
+using Moq;
+using NUnit.Framework;
+using Src.GameplayPresenter.ActionPointsGiving;
+using Src.NetworkingModule;
+
+namespace Tests.EditMode.NetworkManagerTests
+{
+    public class GiveActionPointsAccepterTests
+    {
+        [TestCase(1, 6)]
+        [TestCase(2, 3)]
+        [TestCase(3, 4)]
+        public void AcceptGiveActionPointsDTO_ShouldPassIdAndAmountFromDTO_ToGivenPresenter(int playerId, int amount)
+        {
+            var presenterMock = new Mock<IActionPointsGivingPresenter>();
+            var dto = new GiveActionPointsDTO(playerId, amount);
+            var accepter = new GiveActionPointsAccepter(presenterMock.Object);
+            
+            accepter.AcceptGiveActionPointsDTO(dto);
+            
+            presenterMock.Verify(p => p.GiveActionPoints(playerId, amount), Times.Once);
+        }
+    }
+}
