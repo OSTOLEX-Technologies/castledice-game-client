475ea868bcdedb1f103c17737c833e6d30600f44
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 23:56:17 +0200

    Tested and implemented UnityActionPointsPopup.

---
+++ Assets/Src/GameplayView/ActionPointsGiving/UnityActionPointsPopup.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Src.GameplayView.ActionPointsGiving
+{
+    public class UnityActionPointsPopup : MonoBehaviour, IActionPointsPopup
+    {
+        [SerializeField] private TextMeshProUGUI numberTextMesh;
+        [SerializeField] private TextMeshProUGUI labelTextMesh;
+        
+        public void SetAmount(int amount)
+        {
+            numberTextMesh.text = amount.ToString();
+            labelTextMesh.text = amount == 1 ? "action point" : "action points";
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
