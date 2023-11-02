6a3f4d283717693283e1e5faa6c55fc36c01d809
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 18:31:52 +0200

    Tested and implemented PlayerInitializer.

---
+++ Assets/Tests/EditMode/NetworkManagerTests/PlayerInitializerTests.cs
@@ -0,0 +1,42 @@
+﻿using casltedice_events_logic.ClientToServer;
+using castledice_riptide_dto_adapters.Extensions;
+using NUnit.Framework;
+using Src.NetworkingModule;
+using Tests.Mocks;
+
+namespace Tests.EditMode.NetworkManagerTests
+{
+    public class PlayerInitializerTests
+    {
+        [TestCase("token1")]
+        [TestCase("someToken")]
+        [TestCase("12345")]
+        public void InitializePlayer_ShouldSendMessage_WithInitializePlayerDTO(string playerToken)
+        {
+            var messageSender = new TestMessageSender();
+            var playerInitializer = new PlayerInitializer(messageSender);
+            var expectedDTO = new InitializePlayerDTO(playerToken);
+            
+            playerInitializer.InitializePlayer(playerToken);
+            var message = messageSender.SentMessage;
+            message.GetByte();
+            message.GetByte();
+            var actualDTO = message.GetInitializePlayerDTO();
+            
+            Assert.AreEqual(expectedDTO, actualDTO);
+        }
+
+        [Test]
+        public void InitializePlayer_ShouldSendMessage_WithInitializePlayerMessageId()
+        {
+            var messageSender = new TestMessageSender();
+            var playerInitializer = new PlayerInitializer(messageSender);
+            
+            playerInitializer.InitializePlayer("sometoken");
+            var message = messageSender.SentMessage;
+            var actualMessageId = message.GetUShort();
+            
+            Assert.AreEqual((ushort)ClientToServerMessageType.InitializePlayer, actualMessageId);
+        }
+    }
+}
---
+++ Assets/Tests/Utils/Mocks/TestMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using Riptide;
+using Src.NetworkingModule;
+
+namespace Tests.Mocks
+{
+    public class TestMessageSender : IMessageSender
+    {
+        public Message SentMessage { get; private set; }
+        
+        public void Send(Message message)
+        {
+            SentMessage = message;
+        }
+    }
+}
