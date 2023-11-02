b893e974f3303a3e54478aacef37b17f456a4682
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 22:03:09 +0200

    Wrote message handler for give action points message.

---
+++ Assets/Src/NetworkingModule/DTOAccepters/IGiveActionPointsDTOAccepter.cs
@@ -0,0 +1,9 @@
+﻿using casltedice_events_logic.ServerToClient;
+
+namespace Src.NetworkingModule.DTOAccepters
+{
+    public interface IGiveActionPointsDTOAccepter
+    {
+        void AcceptGiveActionPointsDTO(GiveActionPointsDTO dto);
+    }
+}
---
+++ Assets/Src/NetworkingModule/MessageHandlers/GiveActionPointsMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using casltedice_events_logic.ServerToClient;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+using Src.NetworkingModule.DTOAccepters;
+
+namespace Src.NetworkingModule.MessageHandlers
+{
+    public static class GiveActionPointsMessageHandler
+    {
+        private static IGiveActionPointsDTOAccepter _DTOAccepter;
+        
+        public static void SetAccepter(IGiveActionPointsDTOAccepter accepter)
+        {
+            _DTOAccepter = accepter;
+        }
+        
+        [MessageHandler((ushort) ServerToClientMessageType.GiveActionPoints)]
+        private static void HandleGiveActionPointsMessage(Message message)
+        {
+            _DTOAccepter.AcceptGiveActionPointsDTO(message.GetGiveActionPointsDTO());
+        }
+    }
+}
