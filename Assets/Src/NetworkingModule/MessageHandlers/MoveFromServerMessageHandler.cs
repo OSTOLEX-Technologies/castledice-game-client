9c07f8965ccb6c1c599e1b6aef856aaf03b32136
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 21:34:58 +0200

    Wrote test for ServerMoveAccepter.

---
+++ Assets/Src/NetworkingModule/MessageHandlers/MoveFromServerMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using casltedice_events_logic.ServerToClient;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+using Src.NetworkingModule.DTOAccepters;
+
+namespace Src.NetworkingModule.MessageHandlers
+{
+    public static class MoveFromServerMessageHandler
+    {
+        private static IMoveFromServerDTOAccepter _DTOAccepter;
+        
+        public static void SetDTOAccepter(IMoveFromServerDTOAccepter accepter)
+        {
+            _DTOAccepter = accepter;
+        }
+
+        [MessageHandler((ushort)ServerToClientMessageType.MakeMove)]
+        private static void HandleMoveFromServerMessage(Message message)
+        {
+            _DTOAccepter.AcceptMoveFromServerDTO(message.GetMoveFromServerDTO());
+        }
+    }
+}
