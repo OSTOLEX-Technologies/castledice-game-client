a0886010e867f1f3fd0663c1bf5a2bb038458cff
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-03 00:40:52 +0200

    Made action points popups prefabs.

---
+++ Assets/Src/Stubs/PlayerDataProviderStub.cs
@@ -0,0 +1,31 @@
+﻿using Src.GameplayPresenter;
+
+namespace Src.Stubs
+{
+    //This class MUST NOT be used in a production build.
+    public class PlayerDataProviderStub : IPlayerDataProvider
+    {
+        private int _id;
+
+        public PlayerDataProviderStub()
+        {
+            var rnd = new System.Random();
+            _id = rnd.Next(0, 10000000);
+        }
+
+        public string GetAccessToken()
+        {
+            return _id.ToString();
+        }
+
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public bool IsAuthorized()
+        {
+            return true;
+        }
+    }
+}
