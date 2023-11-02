35e1664543aea1930cf482c5446ecf862b6516e8
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 21:40:24 +0200

    Wrote ActionPointsGiver.

---
+++ Assets/Src/GameplayPresenter/GameWrappers/ActionPointsGiver.cs
@@ -0,0 +1,19 @@
+﻿using castledice_game_logic;
+
+namespace Src.GameplayPresenter.GameWrappers
+{
+    public class ActionPointsGiver : IActionPointsGiver
+    {
+        private readonly Game _game;
+        
+        public ActionPointsGiver(Game game)
+        {
+            _game = game;
+        }
+        
+        public void GiveActionPoints(int playerId, int amount)
+        {
+            _game.GiveActionPointsToPlayer(playerId, amount);
+        }
+    }
+}
