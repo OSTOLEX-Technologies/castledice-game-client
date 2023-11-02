44276ff8fd05a37e032fbf106ba1ede9786d16c7
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-02 22:32:56 +0200

    Wrote test for ActionPointsGivingView.

---
+++ Assets/Src/GameplayView/ActionPointsGiving/ActionPointsGivingView.cs
@@ -0,0 +1,22 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.CellsContent.ContentCreation;
+
+namespace Src.GameplayView.ActionPointsGiving
+{
+    public class ActionPointsGivingView : IActionPointsGivingView
+    {
+        private readonly IPlayerColorProvider _playerColorProvider;
+        private readonly IActionPointsPopupDemonstrator _popupDemonstrator;
+
+        public ActionPointsGivingView(IPlayerColorProvider playerColorProvider, IActionPointsPopupDemonstrator popupDemonstrator)
+        {
+            _playerColorProvider = playerColorProvider;
+            _popupDemonstrator = popupDemonstrator;
+        }
+
+        public void ShowActionPointsForPlayer(Player player, int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Src/GameplayView/ActionPointsGiving/IActionPointsPopupDemonstrator.cs
@@ -0,0 +1,9 @@
+﻿using Src.GameplayView.CellsContent.ContentCreation;
+
+namespace Src.GameplayView.ActionPointsGiving
+{
+    public interface IActionPointsPopupDemonstrator
+    {
+        void ShowActionPointsPopup(PlayerColor playersColor, int amount);
+    }
+}
