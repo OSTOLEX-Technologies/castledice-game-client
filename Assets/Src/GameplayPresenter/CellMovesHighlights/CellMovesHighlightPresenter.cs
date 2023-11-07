733a75ae95e2e36a5ed684473ca2f65da497a7ef
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-07 23:04:28 +0200

    Wrote stub for CellMovesHighlightPresenter.

---
+++ Assets/Src/GameplayPresenter/CellMovesHighlights/CellMovesHighlightPresenter.cs
@@ -0,0 +1,37 @@
+﻿using castledice_game_logic;
+using castledice_game_logic.MovesLogic;
+using Src.GameplayView.CellMovesHighlights;
+
+namespace Src.GameplayPresenter.CellMovesHighlights
+{
+    public class CellMovesHighlightPresenter
+    {
+        private IPlayerDataProvider _playerDataProvider;
+        private ICellMovesListProvider _cellMovesListProvider;
+        private Game _game;
+        private ICellMovesHighlightView _view;
+
+        public CellMovesHighlightPresenter(IPlayerDataProvider playerDataProvider, ICellMovesListProvider cellMovesListProvider, Game game, ICellMovesHighlightView view)
+        {
+            _playerDataProvider = playerDataProvider;
+            _cellMovesListProvider = cellMovesListProvider;
+            _game = game;
+            _view = view;
+        }
+
+        public void HighlightCellMove()
+        {
+            
+        }
+
+        private void OnMoveApplied(object sender, AbstractMove move)
+        {
+            
+        }
+
+        private void OnTurnSwitched(object sender, Game game)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Src/GameplayView/CellMovesHighlights/ICellMovesHighlightView.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using castledice_game_logic.MovesLogic;
+
+namespace Src.GameplayView.CellMovesHighlights
+{
+    public interface ICellMovesHighlightView
+    {
+        void HighlightCellMoves(List<CellMove> cellMoves);
+    }
+}
