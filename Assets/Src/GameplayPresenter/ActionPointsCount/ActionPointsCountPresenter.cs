78ca013af9a040ee915edbe15c2b2bbf85f5c27c
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-09 18:59:14 +0200

    Tested and implemented ActionPointsCountPresenter.

---
+++ Assets/Src/GameplayPresenter/ActionPointsCount/ActionPointsCountPresenter.cs
@@ -0,0 +1,64 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.ActionPointsCount;
+
+namespace Src.GameplayPresenter.ActionPointsCount
+{
+    public class ActionPointsCountPresenter
+    {
+        private readonly IPlayerDataProvider _playerDataProvider;
+        private readonly Game _game;
+        private readonly IActionPointsCountView _view;
+
+        public ActionPointsCountPresenter(IPlayerDataProvider playerDataProvider, Game game, IActionPointsCountView view)
+        {
+            _playerDataProvider = playerDataProvider;
+            _game = game;
+            _view = view;
+            SubscribeToPlayerEvents();
+            SubscribeToGameEvents();
+        }
+
+        private void SubscribeToPlayerEvents()
+        {
+            var player = _game.GetPlayer(_playerDataProvider.GetId());
+            player.ActionPoints.ActionPointsIncreased += OnActionPointsModified;
+            player.ActionPoints.ActionPointsDecreased += OnActionPointsModified;
+        }
+
+        private void SubscribeToGameEvents()
+        {
+            _game.TurnSwitched += OnTurnSwitched;
+        }
+
+        public virtual void UpdateActionPointsCount()
+        {
+            var playerId = _playerDataProvider.GetId();
+            if (!IsCurrentPlayer(playerId))
+            {
+                _view.HideActionPointsCount();
+            }
+            else
+            {
+                var player = _game.GetPlayer(playerId);
+                var actionPointsCount = player.ActionPoints.Amount;
+                _view.ShowActionPointsCount(actionPointsCount);
+            }
+        }
+        
+        private bool IsCurrentPlayer(int playerId)
+        {
+            var currentPlayer = _game.GetCurrentPlayer();
+            return currentPlayer.Id == playerId;
+        }
+
+        private void OnActionPointsModified(object sender, int amount)
+        {
+            UpdateActionPointsCount();
+        }
+
+        private void OnTurnSwitched(object sender, Game game)
+        {
+            UpdateActionPointsCount();
+        }
+    }
+}
