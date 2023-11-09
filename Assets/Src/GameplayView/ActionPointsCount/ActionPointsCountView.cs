945de4ee35326b3b22ddc4c03d1e4ba6a05b0878
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-09 20:35:19 +0200

    Tested and implemented ActionPointsCountView.

---
+++ Assets/Src/GameplayView/ActionPointsCount/ActionPointsCountView.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+
+namespace Src.GameplayView.ActionPointsCount
+{
+    public class ActionPointsCountView : IActionPointsCountView
+    {
+        private readonly TextMeshProUGUI _countTextMesh;
+        private readonly TextMeshProUGUI _labelTextMesh;
+
+        public ActionPointsCountView(TextMeshProUGUI countTextMesh, TextMeshProUGUI labelTextMesh)
+        {
+            _countTextMesh = countTextMesh;
+            _labelTextMesh = labelTextMesh;
+        }
+
+        public void ShowActionPointsCount(int count)
+        {
+            _countTextMesh.gameObject.SetActive(true);
+            _labelTextMesh.gameObject.SetActive(true);
+            _countTextMesh.text = count.ToString();
+        }
+
+        public void HideActionPointsCount()
+        {
+            _countTextMesh.gameObject.SetActive(false);
+            _labelTextMesh.gameObject.SetActive(false);
+        }
+    }
+}
