71a440a3e682a31cf6248b334faf72b9241f6d95
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-10 20:24:16 +0200

    Tested and implemented GameOverView.

---
+++ Assets/Src/GameplayView/GameOver/GameOverView.cs
@@ -0,0 +1,34 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.CellsContent.ContentCreation;
+using UnityEngine;
+
+namespace Src.GameplayView.GameOver
+{
+    public class GameOverView : IGameOverView
+    {
+        private readonly IPlayerColorProvider _playerColorProvider;
+        private readonly GameObject _bluePlayerWinScreen;
+        private readonly GameObject _redPlayerWinScreen;
+        private readonly GameObject _drawScreen;
+
+        public GameOverView(IPlayerColorProvider playerColorProvider, GameObject bluePlayerWinScreen, GameObject redPlayerWinScreen, GameObject drawScreen)
+        {
+            _playerColorProvider = playerColorProvider;
+            _bluePlayerWinScreen = bluePlayerWinScreen;
+            _redPlayerWinScreen = redPlayerWinScreen;
+            _drawScreen = drawScreen;
+        }
+
+        public void ShowWin(Player winner)
+        {
+            var playerColor = _playerColorProvider.GetPlayerColor(winner);
+            _bluePlayerWinScreen.SetActive(playerColor == PlayerColor.Blue);
+            _redPlayerWinScreen.SetActive(playerColor == PlayerColor.Red);
+        }
+
+        public void ShowDraw()
+        {
+            _drawScreen.SetActive(true);
+        }
+    }
+}
