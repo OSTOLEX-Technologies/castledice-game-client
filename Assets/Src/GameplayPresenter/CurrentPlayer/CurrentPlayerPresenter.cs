e3df2a21bab39411991c4c8b11f23d5f41f72616
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-10 16:56:01 +0200

    Wrote stubs for current player presentation logic.

---
+++ Assets/Src/GameplayPresenter/CurrentPlayer/CurrentPlayerPresenter.cs
@@ -0,0 +1,27 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.CurrentPlayer;
+
+namespace Src.GameplayPresenter.CurrentPlayer
+{
+    public class CurrentPlayerPresenter
+    {
+        private readonly Game _game;
+        private readonly ICurrentPlayerView _view;
+
+        public CurrentPlayerPresenter(Game game, ICurrentPlayerView view)
+        {
+            _game = game;
+            _view = view;
+        }
+
+        public virtual void ShowCurrentPlayer()
+        {
+            
+        }
+
+        private void OnTurnSwitched(object sender, Game game)
+        {
+            
+        }
+    }
+}
