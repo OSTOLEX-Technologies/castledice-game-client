ff23bdb7b61ce3585d706ce3f6ab4637e83e631d
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-11 21:57:18 +0200

    Wrote tests for ReadinessSender.

---
+++ Assets/Src/NetworkingModule/ReadinessSender.cs
@@ -0,0 +1,17 @@
+﻿namespace Src.NetworkingModule
+{
+    public class ReadinessSender
+    {
+        private readonly IMessageSender _messageSender;
+        
+        public ReadinessSender(IMessageSender messageSender)
+        {
+            _messageSender = messageSender;
+        }
+
+        public void SendPlayerReadiness(string verificationKey)
+        {
+            
+        }
+    }
+}
