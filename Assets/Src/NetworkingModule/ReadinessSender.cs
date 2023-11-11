aadafd110d643668d7f5ac7da0d65e4f624d555c
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-11 21:59:43 +0200

    Implemented ReadinessSender.

--- Assets/Src/NetworkingModule/ReadinessSender.cs
+++ Assets/Src/NetworkingModule/ReadinessSender.cs
@@ -1,4 +1,8 @@
-﻿namespace Src.NetworkingModule
+﻿using casltedice_events_logic.ClientToServer;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+
+namespace Src.NetworkingModule
 {
     public class ReadinessSender
     {
@@ -11,7 +15,10 @@
 
         public void SendPlayerReadiness(string verificationKey)
         {
-            
+            var message = Message.Create(MessageSendMode.Reliable, (ushort)ClientToServerMessageType.PlayerReady);
+            var DTO = new PlayerReadyDTO(verificationKey);
+            message.AddPlayerReadyDTO(DTO);
+            _messageSender.Send(message);
         }
     }
 }
