e4699f3a369bd9a284e4430ed97a04bf27602fb2
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-14 20:07:38 +0200

    Tested and implemented GameNotSavedErrorView.

---
+++ Assets/Src/GameplayView/Errors/GameNotSavedErrorView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Src.GameplayView.Errors
+{
+    public class GameNotSavedErrorView : IGameNotSavedErrorView
+    {
+        private readonly IErrorPopup _errorPopup;
+        private readonly GameObject _gameCreationProcessScreen;
+
+        public GameNotSavedErrorView(IErrorPopup errorPopup, GameObject gameCreationProcessScreen)
+        {
+            _errorPopup = errorPopup;
+            _gameCreationProcessScreen = gameCreationProcessScreen;
+        }
+
+        public void ShowError(string message)
+        {
+            _errorPopup.SetMessage(message);
+            _errorPopup.Show();
+        }
+
+        public void HideGameCreationProcessScreen()
+        {
+            _gameCreationProcessScreen.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/GameplayViewTests/ErrorsTests/GameNotSavedErrorViewTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using NUnit.Framework;
+using Src.GameplayView.Errors;
+using UnityEngine;
+
+namespace Tests.EditMode.GameplayViewTests.ErrorsTests
+{
+    public class GameNotSavedErrorViewTests
+    {
+        [Test]
+        public void ShowError_ShouldCallShowMethod_OnGivenErrorPopup()
+        {
+            var errorPopupMock = new Mock<IErrorPopup>();
+            var gameCreationProcessScreen = new GameObject();
+            var view = new GameNotSavedErrorView(errorPopupMock.Object, gameCreationProcessScreen);
+
+            view.ShowError("test");
+
+            errorPopupMock.Verify(popup => popup.Show(), Times.Once);
+        }
+
+        [Test]
+        [TestCase("Test error message")]
+        [TestCase("Some other error message")]
+        [TestCase("Some other error message with numbers 1234567890")]
+        public void ShowError_ShouldPassGivenErrorMessage_ToSetMessageMethodOnGivenErrorPopup(string message)
+        {
+            var errorPopupMock = new Mock<IErrorPopup>();
+            var gameCreationProcessScreen = new GameObject();
+            var view = new GameNotSavedErrorView(errorPopupMock.Object, gameCreationProcessScreen);
+
+            view.ShowError(message);
+
+            errorPopupMock.Verify(popup => popup.SetMessage(message), Times.Once);
+        }
+
+        [Test]
+        public void HideGameCreationProcessScreen_ShouldSetActiveFalse_OnGivenGameCreationProcessScreen()
+        {
+            var errorPopupMock = new Mock<IErrorPopup>();
+            var gameCreationProcessScreen = new GameObject();
+            gameCreationProcessScreen.SetActive(true);
+            var view = new GameNotSavedErrorView(errorPopupMock.Object, gameCreationProcessScreen);
+
+            view.HideGameCreationProcessScreen();
+
+            Assert.IsFalse(gameCreationProcessScreen.activeSelf);
+        }
+    }
+}
