bacfdaed64e8ae3c2d3b985fe76293a55bc0fb70
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-14 19:49:14 +0200

    Tested and implemented ErrorPresentersProvider.

---
+++ Assets/Tests/EditMode/GameplayPresenterTests/ErrorsTests/ErrorPresentersProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using castledice_game_data_logic.Errors;
+using Moq;
+using NUnit.Framework;
+using Src.GameplayPresenter.Errors;
+using Src.GameplayView.Errors;
+
+namespace Tests.EditMode.GameplayPresenterTests.ErrorsTests
+{
+    public class ErrorPresentersProviderTests
+    {
+        [Test]
+        public void GetPresenter_ShouldReturnGameNotSavedErrorPresenter_WhenGivenGameNotSavedErrorType()
+        {
+            var expectedPresenter = new GameNotSavedErrorPresenter(new Mock<IGameNotSavedErrorView>().Object);
+            var provider = new ErrorPresentersProvider(expectedPresenter);
+            
+            var presenter = provider.GetPresenter(ErrorType.GameNotSaved);
+            
+            Assert.AreSame(expectedPresenter, presenter);
+        }
+    }
+}
