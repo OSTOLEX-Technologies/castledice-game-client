c6b074907853ab3528447d04420a50e7817fa144
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-25 16:51:41 +0200

    Tested and implemented UnityKnightModelConfig.

---
+++ Assets/Src/GameplayView/CellsContent/ContentCreation/KnightsCreation/UnityKnightModelConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Src.GameplayView.PlayersColors;
+using UnityEngine;
+
+namespace Src.GameplayView.CellsContent.ContentCreation.KnightsCreation
+{
+    public class UnityKnightModelConfig : ScriptableObject, IKnightModelProvider
+    {
+        [SerializeField] private GameObject redKnightModel;
+        [SerializeField] private GameObject blueKnightModel;
+        
+        public GameObject GetKnightModel(PlayerColor color)
+        {
+            switch (color)
+            {
+                case PlayerColor.Red:
+                    return redKnightModel;
+                case PlayerColor.Blue:
+                    return blueKnightModel;
+                default:
+                    throw new ArgumentException("Unknown player color: " + color);
+            }
+        }
+    }
+}
