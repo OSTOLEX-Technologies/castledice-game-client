afcd48a309f8330aa43cda59c81a1724676f6533
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-25 18:17:45 +0200

    Tested and implemented logic for TreeView creation.

---
+++ Assets/Src/GameplayView/CellsContent/ContentCreation/TreesCreation/UnityTreeModelConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Src.GameplayView.CellsContent.ContentCreation.TreesCreation
+{
+    public class UnityTreeModelConfig : ScriptableObject, ITreeModelProvider
+    {
+        [SerializeField] private GameObject treeModel;
+        
+        public GameObject GetTreeModel()
+        {
+            return treeModel;
+        }
+    }
+}
