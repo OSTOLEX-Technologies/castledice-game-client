e86f87f0eefd306f18f8bb5bfb0118d65769c71a
GGLinker <gglinker.channel@gmail.com>
2023-11-29 23:23:18 +0300

    Implemented IAuthView and AuthController entities' base interfaces

---
+++ Assets/Src/AuthController/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using IObjectCacher = Src.Caching.IObjectCacher;
+
+namespace Src.AuthController
+{
+    public class AuthController
+    {
+        private readonly IObjectCacher _cacher;
+        private readonly IAuthView _view;
+
+        public AuthController(IObjectCacher cacher, IAuthView view)
+        {
+            _cacher = cacher;
+            _view = view;
+        }
+    }
+}
--- Assets/Src/AuthController/IAuthView.cs
+++ Assets/Src/AuthController/IAuthView.cs
@@ -2,6 +2,8 @@
 {
     public interface IAuthView
     {
+        public void ShowAuthResult();
+        
         
     }
 }
