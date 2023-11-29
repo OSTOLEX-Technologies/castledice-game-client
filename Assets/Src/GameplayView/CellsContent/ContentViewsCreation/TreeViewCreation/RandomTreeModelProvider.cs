9d781202b461c9b4731da12d5a35734851bb5624
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-29 16:15:25 +0200

    Tested and implemented RandomTreeModelProvider.

--- Assets/Src/GameplayView/CellsContent/ContentViewsCreation/TreeViewCreation/RandomTreeModelProvider.cs
+++ Assets/Src/GameplayView/CellsContent/ContentViewsCreation/TreeViewCreation/RandomTreeModelProvider.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System;
+using castledice_game_logic.Math;
+using UnityEngine;
 
 namespace Src.GameplayView.CellsContent.ContentViewsCreation.TreeViewCreation
 {
     public class RandomTreeModelProvider : ITreeModelProvider
     {
+        private readonly IRangeRandomNumberGenerator _rnd;
+        private readonly ITreeModelPrefabsListProvider _prefabsListProvider;
+        private readonly IInstantiator _instantiator;
+
+        public RandomTreeModelProvider(IRangeRandomNumberGenerator rnd, ITreeModelPrefabsListProvider prefabsListProvider, IInstantiator instantiator)
+        {
+            _rnd = rnd;
+            _prefabsListProvider = prefabsListProvider;
+            _instantiator = instantiator;
+        }
+
         public GameObject GetTreeModel()
         {
-            throw new System.NotImplementedException();
+            var prefabsList = _prefabsListProvider.GetTreeModelPrefabsList();
+            if (prefabsList.Count == 0)
+            {
+                throw new ArgumentException("Tree models prefabs list is empty!");
+            }
+            var index = _rnd.GetRandom(0, prefabsList.Count);
+            return _instantiator.Instantiate(prefabsList[index]);
         }
     }
 }
