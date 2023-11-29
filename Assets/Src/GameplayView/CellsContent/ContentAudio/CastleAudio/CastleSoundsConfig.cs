28c796b8db731e86407b6a1703cf51fe779e6ef1
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-29 23:10:06 +0200

    Tested and implemented CastleSoundsConfig.

---
+++ Assets/Src/GameplayView/CellsContent/ContentAudio/CastleAudio/CastleSoundsConfig.cs
@@ -0,0 +1,24 @@
+﻿using CastleGO = castledice_game_logic.GameObjects.Castle;
+using Src.GameplayView.Audio;
+using UnityEngine;
+
+namespace Src.GameplayView.CellsContent.ContentAudio.CastleAudio
+{
+    [CreateAssetMenu(fileName = "CastleSoundsConfig", menuName = "Configs/Content/Castle/CastleSoundsConfig")]
+
+    public class CastleSoundsConfig : ScriptableObject, ICastleSoundsProvider
+    {
+        [SerializeField] private SoundConfig hitSoundConfig;
+        [SerializeField] private SoundConfig destroySoundConfig;
+        
+        public Sound GetHitSound(CastleGO castle)
+        {
+            return new Sound(hitSoundConfig.clip, hitSoundConfig.volume);
+        }
+
+        public Sound GetDestroySound(CastleGO castle)
+        {
+            return new Sound(destroySoundConfig.clip, destroySoundConfig.volume);
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/GameplayViewTests/CellsContentTests/ContentAudioTests/CastleAudioTests/CastleSoundsConfigTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Src.GameplayView.Audio;
+using Src.GameplayView.CellsContent.ContentAudio.CastleAudio;
+using UnityEngine;
+using static Tests.ObjectCreationUtility;
+
+namespace Tests.EditMode.GameplayViewTests.CellsContentTests.ContentAudioTests.CastleAudioTests
+{
+    public class CastleSoundsConfigTests
+    {
+        [Test]
+        public void GetHitSound_ShouldReturnSound_CreatedAccordingToConfig()
+        {
+            var volume = Random.value;
+            var audioClip = GetAudioClip();
+            var hitSoundConfig = new SoundConfig
+            {
+                clip = audioClip,
+                volume = volume
+            };
+            var castle = GetCastle();
+            var castleSoundsConfig = ScriptableObject.CreateInstance<CastleSoundsConfig>();
+            SetPrivateFieldValue(hitSoundConfig, castleSoundsConfig, "hitSoundConfig");
+            
+            var result = castleSoundsConfig.GetHitSound(castle);
+            
+            Assert.AreSame(audioClip, result.Clip);
+            Assert.AreEqual(volume, result.Volume);
+        }
+        
+        [Test]
+        public void GetDestroySound_ShouldReturnSound_CreatedAccordingToConfig()
+        {
+            var volume = Random.value;
+            var audioClip = GetAudioClip();
+            var destroySoundConfig = new SoundConfig
+            {
+                clip = audioClip,
+                volume = volume
+            };
+            var castle = GetCastle();
+            var castleSoundsConfig = ScriptableObject.CreateInstance<CastleSoundsConfig>();
+            SetPrivateFieldValue(destroySoundConfig, castleSoundsConfig, "destroySoundConfig");
+            
+            var result = castleSoundsConfig.GetDestroySound(castle);
+            
+            Assert.AreSame(audioClip, result.Clip);
+            Assert.AreEqual(volume, result.Volume);
+        }
+    }
+}
