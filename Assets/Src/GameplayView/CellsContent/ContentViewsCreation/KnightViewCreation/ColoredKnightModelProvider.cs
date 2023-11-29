248c9516358aa4f6f097239f02991a3c055a2a3f
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2023-11-29 16:27:01 +0200

    Tested and implemented KnightModelPrefabConfig.

---
+++ Assets/Src/GameplayView/CellsContent/ContentViewsCreation/KnightViewCreation/ColoredKnightModelProvider.cs
@@ -0,0 +1,13 @@
+﻿using castledice_game_logic.GameObjects;
+using UnityEngine;
+
+namespace Src.GameplayView.CellsContent.ContentViewsCreation.KnightViewCreation
+{
+    public class ColoredKnightModelProvider : IKnightModelProvider
+    {
+        public GameObject GetKnightModel(Knight knight)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
