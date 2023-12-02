514bc1c87bc7f11e501fdc64f56ded9e609d50cb
GGLinker <gglinker.channel@gmail.com>
2023-12-02 21:29:37 +0300

    Secured authorization credentials

--- Assets/Src/AuthController/CredentialProviders/GoogleCredentialProvider.cs
+++ Assets/Src/AuthController/CredentialProviders/GoogleCredentialProvider.cs
@@ -4,17 +4,13 @@ using System.Threading.Tasks;
 using Proyecto26;
 using Src.AuthController.REST;
 using Src.AuthController.REST.REST_Responses;
+using Src.AuthController.AuthKeys;
 using UnityEngine;
 
 namespace Src.AuthController.CredentialProviders
 {
     public static class GoogleCredentialProvider
     {
-        private const string ApiKey = "AIzaSyDJKKgNOH6a0HRVpkR4rn42eAgqF8Umrso";
-        
-        private const string ClientId = "150757942999-m2nn366uakk12l8clf3pj7rfuvj6a04v.apps.googleusercontent.com";
-        private const string ClientSecret = "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";
-        
         private const int LoopbackPort = 3303;
         private static readonly string RedirectUri = $"http://localhost:{LoopbackPort}";
 
@@ -59,7 +55,7 @@ namespace Src.AuthController.CredentialProviders
         
         private static void GetAuthData(TaskCompletionSource<GoogleIdTokenResponse> tcs)
         {
-            Application.OpenURL($"https://accounts.google.com/o/oauth2/v2/auth?client_id={ClientId}&redirect_uri={RedirectUri}&response_type=code&scope=email");
+            Application.OpenURL($"https://accounts.google.com/o/oauth2/v2/auth?client_id={GoogleAuthConfig.ClientId}&redirect_uri={RedirectUri}&response_type=code&scope=email");
 
             Listener.StartListening(code =>
             {
@@ -85,8 +81,8 @@ namespace Src.AuthController.CredentialProviders
                 Params = new Dictionary<string, string>
                 {
                     {"code", _authCode},
-                    {"client_id", ClientId},
-                    {"client_secret", ClientSecret},
+                    {"client_id", GoogleAuthConfig.ClientId},
+                    {"client_secret", GoogleAuthConfig.ClientSecret},
                     {"redirect_uri", RedirectUri},
                     {"grant_type","authorization_code"}
                 }
@@ -108,8 +104,8 @@ namespace Src.AuthController.CredentialProviders
                 Params = new Dictionary<string, string>
                 {
                     {"code", _authCode},
-                    {"client_id", ClientId},
-                    {"client_secret", ClientSecret},
+                    {"client_id", GoogleAuthConfig.ClientId},
+                    {"client_secret", GoogleAuthConfig.ClientSecret},
                     {"redirect_uri", RedirectUri},
                     {"grant_type","authorization_code"}
                 }
@@ -124,18 +120,5 @@ namespace Src.AuthController.CredentialProviders
                     tcs.SetResult(_googleApiResponse);
                 }).Catch(Debug.Log);
         }
-        
-
-        
-        /*private static void SingInWithToken(string token, string providerId)
-        {
-            var payLoad =
-                $"{{\"postBody\":\"id_token={token}&providerId={providerId}\",\"requestUri\":\"http://localhost\",\"returnIdpCredential\":true,\"returnSecureToken\":true}}";
-            RestClient.Post($"https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key={ApiKey}", payLoad).Then(
-                response =>
-                {
-                    Debug.Log(response.Text);
-                }).Catch(Debug.Log);
-        }*/
     }
 }
