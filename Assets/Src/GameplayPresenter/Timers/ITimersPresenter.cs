2521f8e81fed61ed8d9b474bf15a362aea5648f6
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-05 18:52:24 +0200

    Created interfaces for timers controllers and views.

---
+++ Assets/Src/GameplayPresenter/Timers/ITimersPresenter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Src.GameplayPresenter.Timers
+{
+    public interface ITimersPresenter
+    {/// <summary>
+     /// 
+     /// </summary>
+     /// <param name="playerId"></param>
+     /// <param name="timeLeft"></param>
+     /// <param name="switchTo">If true, than timer should be started. If false, than timer should be stopped.</param>
+        void SwitchTimerForPlayer(int playerId, TimeSpan timeLeft, bool switchTo);
+    }
+}
---
+++ Assets/Src/GameplayView/Timers/ITimersView.cs
@@ -0,0 +1,10 @@
+﻿using castledice_game_logic;
+
+namespace Src.GameplayView.Timers
+{
+    public interface ITimersView
+    {
+        void StartTimerForPlayer(Player player);
+        void StopTimerForPlayer(Player player);
+    }
+}
