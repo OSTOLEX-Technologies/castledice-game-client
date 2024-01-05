e8f6c49ed7a1e2ca749bea75e1abdb0511c5ed80
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-05 19:36:46 +0200

    Wrote message handler for switch timer message.

---
+++ Assets/Src/NetworkingModule/MessageHandlers/SwitchTimerMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using castledice_events_logic.ServerToClient;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
+using Src.NetworkingModule.DTOAccepters;
+
+namespace Src.NetworkingModule.MessageHandlers
+{
+    public static class SwitchTimerMessageHandler
+    {
+        private static ISwitchTimerDTOAccepter _DTOAccepter;
+        
+        public static void SetAccepter(ISwitchTimerDTOAccepter accepter)
+        {
+            _DTOAccepter = accepter;
+        }
+
+        [MessageHandler((ushort)ServerToClientMessageType.SwitchTimer)]
+        private static void HandleSwitchTimerMessage(Message message)
+        {
+            var dto = message.GetSwitchTimerDTO();
+            _DTOAccepter.AcceptSwitchTimerDTO(dto);
+        }
+    }
+}
