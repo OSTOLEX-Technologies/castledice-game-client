b91ccc0ac5763b5ed68914c4ffc535b51c3eaa84
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-05 22:17:22 +0200

    Tested and implemented PlayerTimerView.

---
+++ Assets/Src/GameplayView/Timers/PlayerTimerViews/PlayerTimerView.cs
@@ -0,0 +1,33 @@
+﻿using castledice_game_logic.Time;
+
+namespace Src.GameplayView.Timers.PlayerTimerViews
+{
+    public class PlayerTimerView : IPlayerTimerView
+    {
+        private readonly TimeView _timeView;
+        private readonly Highlighter _highlighter;
+        private readonly IPlayerTimer _playerTimer;
+        
+        public PlayerTimerView(TimeView timeView, Highlighter highlighter, IPlayerTimer playerTimer)
+        {
+            _timeView = timeView;
+            _highlighter = highlighter;
+            _playerTimer = playerTimer;
+        }
+
+        public void Update()
+        {
+            _timeView.SetTime(_playerTimer.GetTimeLeft());
+        }
+
+        public void Highlight()
+        {
+            _highlighter.Highlight();
+        }
+
+        public void Obscure()
+        {
+            _highlighter.Obscure();
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/GameplayViewTests/TimersViewTests/PlayerTimerViewsTests/PlayerTimerViewTests.cs
@@ -0,0 +1,67 @@
+﻿using castledice_game_logic.Time;
+using Moq;
+using NUnit.Framework;
+using Src.GameplayView.Timers.PlayerTimerViews;
+
+namespace Tests.EditMode.GameplayViewTests.TimersViewTests.PlayerTimerViewsTests
+{
+    public class PlayerTimerViewTests
+    {
+        [Test]
+        public void Highlight_ShouldCallHighlight_OnHighlighter()
+        {
+            var highlighterMock = new Mock<Highlighter>();
+            var playerTimerView = new PlayerTimerViewBuilder
+            {
+                Highlighter = highlighterMock.Object
+            }.Build();
+            
+            playerTimerView.Highlight();
+            
+            highlighterMock.Verify(highlighter => highlighter.Highlight(), Times.Once);
+        }
+        
+        [Test]
+        public void Obscure_ShouldCallObscure_OnHighlighter()
+        {
+            var highlighterMock = new Mock<Highlighter>();
+            var playerTimerView = new PlayerTimerViewBuilder
+            {
+                Highlighter = highlighterMock.Object
+            }.Build();
+            
+            playerTimerView.Obscure();
+            
+            highlighterMock.Verify(highlighter => highlighter.Obscure(), Times.Once);
+        }
+        
+        [Test]
+        public void Update_ShouldSetTimeLeftFromTimer_ToTimeView()
+        {
+            var timeViewMock = new Mock<TimeView>();
+            var playerTimerMock = new Mock<IPlayerTimer>();
+            var playerTimer = playerTimerMock.Object;
+            var playerTimerView = new PlayerTimerViewBuilder
+            {
+                TimeView = timeViewMock.Object,
+                PlayerTimer = playerTimer
+            }.Build();
+            
+            playerTimerView.Update();
+            
+            timeViewMock.Verify(timeView => timeView.SetTime(playerTimer.GetTimeLeft()), Times.Once);
+        }
+        
+        private class PlayerTimerViewBuilder
+        {
+            public TimeView TimeView { get; set; } = new Mock<TimeView>().Object;
+            public Highlighter Highlighter { get; set; } = new Mock<Highlighter>().Object;
+            public IPlayerTimer PlayerTimer { get; set; } = new Mock<IPlayerTimer>().Object;
+            
+            public PlayerTimerView Build()
+            {
+                return new PlayerTimerView(TimeView, Highlighter, PlayerTimer);
+            }
+        }
+    }
+}
