cecc3c14509d7db46dd5f4cf9b030d1007d33b79
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-05 20:20:59 +0200

    Created abstract classes for player timer view and implemented TextMeshTimeView.

---
+++ Assets/Src/GameplayView/Timers/PlayerTimerViews/Highlighter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Src.GameplayView.Timers.PlayerTimerViews
+{
+    public abstract class Highlighter : MonoBehaviour
+    {
+        public abstract void Highlight();
+        public abstract void Obscure();
+    }
+}
