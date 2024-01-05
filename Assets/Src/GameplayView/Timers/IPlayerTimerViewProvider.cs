fefb614f763d63930b873037a2f05c0a2738e232
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-05 22:26:44 +0200

    Tested and implemented TimersView.

---
+++ Assets/Src/GameplayView/Timers/IPlayerTimerViewProvider.cs
@@ -0,0 +1,10 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.Timers.PlayerTimerViews;
+
+namespace Src.GameplayView.Timers
+{
+    public interface IPlayerTimerViewProvider
+    {
+        IPlayerTimerView GetTimerViewForPlayer(Player player);
+    }
+}
---
+++ Assets/Src/GameplayView/Timers/TimersView.cs
@@ -0,0 +1,29 @@
+﻿using castledice_game_logic;
+using Src.GameplayView.Updatables;
+
+namespace Src.GameplayView.Timers
+{
+    public class TimersView : ITimersView
+    {
+        private readonly IPlayerTimerViewProvider _playerTimerViewProvider;
+        private readonly IUpdater _updater;
+        
+        public TimersView(IPlayerTimerViewProvider playerTimerViewProvider, IUpdater updater)
+        {
+            _playerTimerViewProvider = playerTimerViewProvider;
+            _updater = updater;
+        }
+        
+        public void StartTimer(Player player)
+        {
+            var playerTimerView = _playerTimerViewProvider.GetTimerViewForPlayer(player);
+            _updater.AddUpdatable(playerTimerView);
+        }
+
+        public void StopTimer(Player player)
+        {
+            var playerTimerView = _playerTimerViewProvider.GetTimerViewForPlayer(player);
+            _updater.RemoveUpdatable(playerTimerView);
+        }
+    }
+}
