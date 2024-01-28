0aad21182c5934730cb4eeabd47814deb6795060
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-01-28 14:39:22 +0200

    Tested and implemented logic for new units highlights.

---
+++ Assets/Src/GameplayPresenter/NewUnitsHighlights/NewUnitsHighlightsPresenter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using castledice_game_logic;
+using castledice_game_logic.GameObjects;
+using castledice_game_logic.Math;
+using castledice_game_logic.MovesLogic;
+using Src.GameplayView.NewUnitsHighlights;
+
+namespace Src.GameplayPresenter.NewUnitsHighlights
+{
+    public class NewUnitsHighlightsPresenter
+    {
+        private readonly Game _game;
+        private readonly List<Vector2Int> _newUnitsPositions = new();
+        private readonly INewUnitsHighlightsView _view;
+
+        public NewUnitsHighlightsPresenter(Game game, INewUnitsHighlightsView view)
+        {
+            _game = game;
+            _view = view;
+            _game.TurnSwitched += OnTurnSwitched;
+            _game.MoveApplied += OnMoveApplied;
+            foreach (var cell in _game.GetBoard())
+            {
+                cell.ContentAdded += OnContentAdded;
+            }
+        }
+
+        private void OnContentAdded(object sender, Content content)
+        {
+            if (content is IPlayerOwned)
+            {
+                _newUnitsPositions.Add(((Cell)sender).Position);
+            }
+        }
+
+        private void OnTurnSwitched(object sender, Game game)
+        {
+            foreach (var pos in _newUnitsPositions)
+            {
+                _view.ShowHighlight(pos, game.GetPreviousPlayer());
+            }
+            _newUnitsPositions.Clear();
+        }
+        
+        private void OnMoveApplied(object sender, AbstractMove move)
+        {
+            _view.HideHighlights();
+        }
+        
+        ~NewUnitsHighlightsPresenter()
+        {
+            _game.TurnSwitched -= OnTurnSwitched;
+            _game.MoveApplied -= OnMoveApplied;
+            foreach (var cell in _game.GetBoard())
+            {
+                cell.ContentAdded -= OnContentAdded;
+            }
+        }
+    }
+}
---
+++ Assets/Src/GameplayView/NewUnitsHighlights/NewUnitsHighlightsView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using castledice_game_logic;
+using castledice_game_logic.Math;
+using Src.GameplayView.Grid;
+using Src.GameplayView.Highlights;
+using Src.GameplayView.PlayerObjectsColor;
+
+namespace Src.GameplayView.NewUnitsHighlights
+{
+    public class NewUnitsHighlightsView : INewUnitsHighlightsView
+    {
+        private readonly IPlayerObjectsColorProvider _colorProvider;
+        private readonly Dictionary<Vector2Int, ColoredHighlight> _highlights = new();
+
+        public NewUnitsHighlightsView(IGrid grid, IColoredHighlightCreator highlightCreator, IPlayerObjectsColorProvider colorProvider)
+        {
+            _colorProvider = colorProvider;
+            foreach (var cell in grid)
+            {
+                var highlight = highlightCreator.GetHighlight();
+                var position = cell.Position;
+                cell.AddChild(highlight.gameObject);
+                _highlights.Add(position, highlight);
+                highlight.Hide();
+            }
+        }
+
+        public void ShowHighlight(Vector2Int position, Player player)
+        {
+            if (!_highlights.ContainsKey(position))
+            {
+                throw new InvalidOperationException("No highlight on given position: " + position);
+            }
+
+            var color = _colorProvider.GetColor(player);
+            var highlight = _highlights[position];
+            highlight.Show();
+            highlight.SetColor(color);
+        }
+
+        public void HideHighlights()
+        {
+            foreach (var highlight in _highlights.Values)
+            {
+                highlight.Hide();
+            }
+        }
+    }
+}
