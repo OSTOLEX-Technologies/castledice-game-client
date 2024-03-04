4e0f8a43e0495c3da6cc6cab06d057c0bbeb7cdd
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-03-04 14:45:48 +0200

    Tested and implemented ReachCostCalculator.

---
+++ Assets/Src/PVE/Calculators/IBoardCostsCalculator.cs
@@ -0,0 +1,15 @@
+﻿using castledice_game_logic;
+using castledice_game_logic.MovesLogic;
+
+namespace Src.PVE.Calculators
+{
+    /// <summary>
+    /// Methods of this interface should return matrix each cell of which contains the cost of performing a move to the cell.
+    /// If no move is possible on the cell, then the cost should be int.MaxValue.
+    /// </summary>
+    public interface IBoardCostsCalculator
+    {
+        int[,] GetCosts(Player forPlayer);
+        int[,] GetCostsAfterMove(Player forPlayer, AbstractMove afterMove);
+    }
+}
---
+++ Assets/Src/PVE/Calculators/ReachCostCalculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using castledice_game_logic;
+using castledice_game_logic.Math;
+using castledice_game_logic.MovesLogic;
+
+namespace Src.PVE.Calculators
+{
+    public class ReachCostCalculator : IReachCostCalculator
+    {
+        private readonly IBoardCostsCalculator _costsCalculator;
+        private readonly IOccupiedPositionsCalculator _occupiedPositionsCalculator;
+        private readonly IMatrixPathMinCostSearcher _minCostSearcher;
+
+        public ReachCostCalculator(IBoardCostsCalculator costsCalculator, IOccupiedPositionsCalculator occupiedPositionsCalculator, IMatrixPathMinCostSearcher minCostSearcher)
+        {
+            _costsCalculator = costsCalculator;
+            _occupiedPositionsCalculator = occupiedPositionsCalculator;
+            _minCostSearcher = minCostSearcher;
+        }
+
+        public int GetMinReachCost(Player forPlayer, Vector2Int toPosition)
+        {
+            var occupiedPositions = _occupiedPositionsCalculator.GetOccupiedPositions(forPlayer);
+            if (occupiedPositions.Count == 0) return int.MaxValue;
+            var costsMatrix = _costsCalculator.GetCosts(forPlayer);
+            var costs = new int[occupiedPositions.Count];
+            for (var i = 0; i < occupiedPositions.Count; i++)
+            {
+                costs[i] = _minCostSearcher.GetMinCost(costsMatrix, occupiedPositions[i], toPosition);
+            }
+            return costs.Min();
+        }
+
+        public int GetMinReachCostAfterMove(Player forPlayer, Vector2Int toPosition, AbstractMove afterMove)
+        {
+            var occupiedPositions = _occupiedPositionsCalculator.GetOccupiedPositionsAfterMove(forPlayer, afterMove);
+            if (occupiedPositions.Count == 0) return int.MaxValue;
+            var costsMatrix = _costsCalculator.GetCostsAfterMove(forPlayer, afterMove);
+            var costs = new int[occupiedPositions.Count];
+            for (var i = 0; i < occupiedPositions.Count; i++)
+            {
+                costs[i] = _minCostSearcher.GetMinCost(costsMatrix, occupiedPositions[i], toPosition);
+            }
+            return costs.Min();
+        }
+    }
+}
