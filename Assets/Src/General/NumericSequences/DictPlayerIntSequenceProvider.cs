7c4daeb23a4fcb3fc1413ce694868a34c522ca4e
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-03-15 14:05:48 +0200

    Wrote first test for DictPlayerIntSequenceProvider.

---
+++ Assets/Src/General/NumericSequences/DictPlayerIntSequenceProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using castledice_game_logic;
+using Dynamitey.DynamicObjects;
+
+namespace Src.General.NumericSequences
+{
+    public class DictPlayerIntSequenceProvider : IPlayerIntSequenceProvider
+    {
+        private readonly Dictionary<Player, IIntSequence> _dictionary;
+
+        public DictPlayerIntSequenceProvider(Dictionary<Player, IIntSequence> dictionary)
+        {
+            _dictionary = dictionary;
+        }
+        
+        public IIntSequence GetSequence(Player forPlayer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Tests/EditMode/GeneralTests/NumericSequencesTests/DictPlayerIntSequenceProviderTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using castledice_game_logic;
+using Moq;
+using NUnit.Framework;
+using Src.General.NumericSequences;
+
+namespace Tests.EditMode.GeneralTests.NumericSequencesTests
+{
+    public class DictPlayerIntSequenceProviderTests
+    {
+        [Test]
+        public void GetSequence_ShouldThrowInvalidOperationException_IfDictionaryIsEmpty()
+        {
+            var dictionary = new Dictionary<Player, IIntSequence>();
+            var provider = new DictPlayerIntSequenceProvider(dictionary);
+
+            Assert.Throws<InvalidOperationException>(() => provider.GetSequence(It.IsAny<Player>()));
+        }
+    }
+}
