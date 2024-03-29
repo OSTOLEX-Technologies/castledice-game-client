d4da8e055a25454f060940db6a6a5d325f1fded5
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-03-29 18:56:24 +0300

    Added ServerConnectionConfig.

---
+++ Assets/Src/GameplayPresenter/ServerConnection/ServerConnectionConfig.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Src.GameplayPresenter.ServerConnection
+{
+    public class ServerConnectionConfig : ScriptableObject, IServerConnectionConfig 
+    {
+        [SerializeField] private string hostAddress;
+        [SerializeField] private int maxConnectionAttempts;
+
+        public string HostAddress => hostAddress;
+
+        public int MaxConnectionAttempts => maxConnectionAttempts;
+    }
+}
