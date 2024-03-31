8d74265ddf6775c4cfa149e1712f99edb6c1ecc6
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-03-31 19:23:36 +0300

    Made popups for connection process.

---
+++ Assets/Src/General/StringsListConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Src.General
+{
+    [CreateAssetMenu(menuName = "Configs/StringsListConfig", fileName = "StringsListConfig", order = 0)]
+    public class StringsListConfig : ScriptableObject
+    {
+        [SerializeField] private List<string> strings;
+        
+        public List<string> Strings => strings;
+    }
+}
