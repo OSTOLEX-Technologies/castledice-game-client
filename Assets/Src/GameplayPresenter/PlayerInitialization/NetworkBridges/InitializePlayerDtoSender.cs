9b29a4851ae06eca22a175208c38b186ad7fcf94
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-04-09 17:11:39 +0300

    Removed deprecated classes.

--- Assets/Src/GameplayPresenter/PlayerInitialization/NetworkBridges/InitializePlayerDtoSender.cs
+++ Assets/Src/GameplayPresenter/PlayerInitialization/NetworkBridges/InitializePlayerDtoSender.cs
@@ -1,4 +1,6 @@
 using castledice_events_logic.ClientToServer;
+using castledice_riptide_dto_adapters.Extensions;
+using Riptide;
 using Src.NetworkingModule;
 
 namespace Src.GameplayPresenter.PlayerInitialization.NetworkBridges
@@ -6,10 +8,17 @@ namespace Src.GameplayPresenter.PlayerInitialization.NetworkBridges
     public class InitializePlayerDtoSender : IInitializePlayerDtoSender
     {
         private readonly IMessageSender _messageSender;
-        
+
+        public InitializePlayerDtoSender(IMessageSender messageSender)
+        {
+            _messageSender = messageSender;
+        }
+
         public void SendDto(InitializePlayerDTO dto)
         {
-            
+            var message = Message.Create(MessageSendMode.Unreliable, ClientToServerMessageType.CancelGame);
+            message.AddInitializePlayerDTO(dto);
+            _messageSender.Send(message);
         }
     }
 }
---
+++ Assets/Tests/EditMode/GameplayPresenterTests/PlayerInitializationTests/NetworkBridgesTests/InitializePlayerDtoSenderTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using castledice_events_logic.ClientToServer;
+using castledice_riptide_dto_adapters.Extensions;
+using Moq;
+using NUnit.Framework;
+using Riptide;
+using Src.GameplayPresenter.PlayerInitialization.NetworkBridges;
+using Src.NetworkingModule;
+
+namespace Tests.EditMode.GameplayPresenterTests.PlayerInitializationTests.NetworkBridgesTests
+{
+    public class InitializePlayerDtoSenderTests
+    {
+        [Test]
+        public void SendDto_ShouldSendMessage_WithGivenDto()
+        {
+            var expectedDto = new InitializePlayerDTO(new Random().Next().ToString());
+            var messageSenderMock = new Mock<IMessageSender>();
+            Message sentMessage = null;
+            messageSenderMock.Setup(x => x.Send(It.IsAny<Message>()))
+                .Callback<Message>(message => sentMessage = message);
+            var sender = new InitializePlayerDtoSender(messageSenderMock.Object);
+            
+            sender.SendDto(expectedDto);
+            sentMessage.GetByte();
+            var actualDto = sentMessage.GetInitializePlayerDTO();
+            
+            Assert.AreEqual(expectedDto.VerificationKey, actualDto.VerificationKey);
+        }
+
+        [Test]
+        public void SendDto_ShouldSendMessage_WithReliableSendMode()
+        {
+            var messageSenderMock = new Mock<IMessageSender>();
+            Message sentMessage = null;
+            messageSenderMock.Setup(x => x.Send(It.IsAny<Message>()))
+                .Callback<Message>(message => sentMessage = message);
+            var sender = new InitializePlayerDtoSender(messageSenderMock.Object);
+            
+            sender.SendDto(new InitializePlayerDTO("test"));
+            
+            Assert.AreEqual(MessageSendMode.Reliable, sentMessage.SendMode);
+        }
+
+        [Test]
+        public void SendDto_ShouldSendMessage_WithInitializePlayerMessageType()
+        {
+            var messageSenderMock = new Mock<IMessageSender>();
+            Message sentMessage = null;
+            messageSenderMock.Setup(x => x.Send(It.IsAny<Message>()))
+                .Callback<Message>(message => sentMessage = message);
+            var sender = new InitializePlayerDtoSender(messageSenderMock.Object);
+            
+            sender.SendDto(new InitializePlayerDTO("test"));
+            var actualMessageType = sentMessage.GetByte();
+            
+            Assert.AreEqual(ClientToServerMessageType.InitializePlayer, actualMessageType);
+        }
+    }
+}
