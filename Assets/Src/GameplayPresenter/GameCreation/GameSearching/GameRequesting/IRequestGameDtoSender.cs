d00700419a60578366f3a06f03fcc9a0f701d6f5
Mehanari <102820106+Mehanari@users.noreply.github.com>
2024-04-17 15:46:01 +0300

    Wrote and tested GameRequester.

---
+++ Assets/Src/GameplayPresenter/GameCreation/GameSearching/GameRequesting/IRequestGameDtoSender.cs
@@ -0,0 +1,9 @@
+﻿using castledice_events_logic.ClientToServer;
+
+namespace Src.GameplayPresenter.GameCreation.GameSearching.GameRequesting
+{
+    public interface IRequestGameDtoSender
+    {
+        public void SendDto(RequestGameDTO dto);
+    }
+}
