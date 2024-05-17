84ce5dc40e34dbaec348d4c6af8fbaf208e4f472
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-05-17 10:05:06 +0300

    Made BotMoveCommand without undo.

---
+++ Assets/Src/PVE/BotTriggers/MonoBehaviourBotMoveTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Src.PVE.BotTriggers
+{
+    public class MonoBehaviourBotMoveTrigger : IBotMoveTrigger
+    {
+        public event Action ShouldMakeMove;
+        
+        public void TriggerBot()
+        {
+            ShouldMakeMove?.Invoke();
+        }
+    }
+}
---
+++ Assets/Src/TutorialScenario/ScenarioCommands/BotMoveCommand.cs
@@ -0,0 +1,20 @@
+﻿using Src.PVE.BotTriggers;
+using UnityEngine;
+
+namespace Src.TutorialScenario.ScenarioCommands
+{
+    public class BotMoveCommand : TutorialScenarioCommand
+    {
+        [SerializeField] private MonoBehaviourBotMoveTrigger _botMoveTrigger;
+        
+        public override void Do()
+        {
+            _botMoveTrigger.TriggerBot();
+        }
+
+        public override void Undo()
+        {
+            Debug.LogWarning("Undoing bot move command is not supported");
+        }
+    }
+}
