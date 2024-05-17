0c6c9b59e55b9a9df5e7b57e3c698532e8573c8b
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-05-17 10:59:26 +0300

    Created ChangeActionPointsCommand.

---
+++ Assets/Src/Tutorial/ActionPointsGiving/TutorialActionPointsGiver.cs
@@ -0,0 +1,25 @@
+﻿using castledice_game_logic;
+using UnityEngine;
+
+namespace Src.Tutorial.ActionPointsGiving
+{
+    public class TutorialActionPointsChanger : MonoBehaviour
+    {
+        private Game _game;
+
+        public void Init(Game game)
+        {
+            _game = game;
+        }
+        
+        public void GiveActionPointsToPlayer(int playerId, int actionPointsAmount)
+        {
+            _game.GiveActionPointsToPlayer(playerId, actionPointsAmount);
+        }
+        
+        public void TakeActionPointsFromPlayer(int playerId, int actionPointsAmount)
+        {
+            _game.TakeActionPointsFromPlayer(playerId, actionPointsAmount);
+        }
+    }
+}
---
+++ Assets/Src/TutorialScenario/ScenarioCommands/ChangeActionPointsCommand.cs
@@ -0,0 +1,46 @@
+﻿using Src.Tutorial.ActionPointsGiving;
+using UnityEngine;
+
+namespace Src.TutorialScenario.ScenarioCommands
+{
+    public class ChangeActionPointsCommand : TutorialScenarioCommand 
+    {
+        private enum ActionType
+        {
+            Give,
+            Take
+        }
+        
+        [SerializeField] private int playerId;
+        [SerializeField] private int actionPointsAmount;
+        [SerializeField] private ActionType actionType;
+        [SerializeField] private TutorialActionPointsChanger tutorialActionPointsChanger;
+        private int _cachedActionPointsChange;
+        
+        public override void Do()
+        {
+            if (actionType == ActionType.Give)
+            {
+                _cachedActionPointsChange = actionPointsAmount;
+                tutorialActionPointsChanger.GiveActionPointsToPlayer(playerId, actionPointsAmount);
+            }
+            else
+            {
+                _cachedActionPointsChange = -actionPointsAmount;
+                tutorialActionPointsChanger.TakeActionPointsFromPlayer(playerId, actionPointsAmount);
+            }
+        }
+
+        public override void Undo()
+        {
+            if (actionType == ActionType.Give)
+            {
+                tutorialActionPointsChanger.TakeActionPointsFromPlayer(playerId, _cachedActionPointsChange);
+            }
+            else
+            {
+                tutorialActionPointsChanger.GiveActionPointsToPlayer(playerId, -_cachedActionPointsChange);
+            }
+        }
+    }
+}
