627fd961ff81ab8a61dca1c9a8f5f559ffd8c133
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-05-17 11:31:07 +0300

    Added SetTextCommand.

---
+++ Assets/Src/TutorialScenario/ScenarioCommands/SetTextCommand.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Src.TutorialScenario.ScenarioCommands
+{
+    public class SetTextCommand : TutorialScenarioCommand
+    {
+        [SerializeField] private TextMeshProUGUI textMesh;
+        [SerializeField] private string textToSet;
+        private string _cachedText;
+        
+        public override void Do()
+        {
+            _cachedText = textMesh.text;
+            textMesh.text = textToSet;
+        }
+
+        public override void Undo()
+        {
+            textMesh.text = _cachedText;
+        }
+    }
+}
