ab7f6cf3e8b831c0b65949f634f3306199cdc103
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-05-18 12:15:52 +0300

    Made command for cell moves highlight toggling.

---
+++ Assets/Src/GameplayView/CellMovesHighlights/CellMovesHighlightsToggle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Src.GameplayView.CellMovesHighlights
+{
+    public class UnityCellMovesHighlightsToggle : MonoBehaviour
+    {
+        private List<UnityCellMoveHighlight> _highlights;
+        
+        public void Init(List<UnityCellMoveHighlight> highlights)
+        {
+            _highlights = highlights;
+        }
+        
+        public void SetHighlightsActive(bool value)
+        {
+            foreach (var highlight in _highlights)
+            {
+                highlight.gameObject.SetActive(value);
+            }
+        }
+    }
+}
---
+++ Assets/Src/TutorialScenario/ScenarioCommands/ToggleCellMovesHighlightsCommand.cs
@@ -0,0 +1,21 @@
+﻿using Src.GameplayView.CellMovesHighlights;
+using UnityEngine;
+
+namespace Src.TutorialScenario.ScenarioCommands
+{
+    public class ToggleCellMovesHighlightsCommand : TutorialScenarioCommand
+    {
+        [SerializeField] private UnityCellMovesHighlightsToggle toggle;
+        [SerializeField] private bool active;
+        
+        public override void Do()
+        {
+            toggle.SetHighlightsActive(active);
+        }
+
+        public override void Undo()
+        {
+            toggle.SetHighlightsActive(!active);
+        }
+    }
+}
